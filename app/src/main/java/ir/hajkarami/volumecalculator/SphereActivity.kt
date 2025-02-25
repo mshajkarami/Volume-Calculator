34acf95c7bf2fc8e30477a5ab9a6fcccc9985224
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-25 13:29:23 +0330

    finish step 1 of app

--- app/src/main/java/ir/hajkarami/volumecalculator/SphereActivity.kt
+++ app/src/main/java/ir/hajkarami/volumecalculator/SphereActivity.kt
@@ -3,19 +3,30 @@ package ir.hajkarami.volumecalculator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ir.hajkarami.volumecalculator.databinding.ActivitySphereBinding
 
 class SphereActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySphereBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySphereBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sphere)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.apply {
+            btnCalculate.setOnClickListener {
+                val radius = binding.inputEditTest.text.toString()
+                val r = radius.toDoubleOrNull()
+
+                if (r == null || r <= 0) {
+                    textResult.text = "Please enter a valid value."
+                    return@setOnClickListener
+                }
+
+                val volume = (4.0 / 3.0) * Math.PI * r * r * r
+                textResult.text = "V = %.2f m³".format(volume)
+            }
         }
     }
 }
