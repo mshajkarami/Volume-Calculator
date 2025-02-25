7a859b91dcdf07b81a163bcadb5d8ac09860521d
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-25 10:53:28 +0330

    init Project with the model class

---
+++ app/src/main/java/ir/hajkarami/volumecalculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package ir.hajkarami.volumecalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ir.hajkarami.volumecalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/ir/hajkarami/volumecalculator/Shape.kt
@@ -0,0 +1,6 @@
+package ir.hajkarami.volumecalculator
+
+data class Shape(
+    val name: String,
+    val image: Int
+)
