dfa72cbeab861762611f1b78bbd0c56fd133e045
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-25 12:38:13 +0330

    Finish GridView Of Layout

--- app/src/main/java/ir/hajkarami/volumecalculator/MainActivity.kt
+++ app/src/main/java/ir/hajkarami/volumecalculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package ir.hajkarami.volumecalculator
 
 import android.os.Bundle
+import android.widget.GridView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,11 +9,37 @@ import androidx.core.view.WindowInsetsCompat
 import ir.hajkarami.volumecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var shapeArrayList: ArrayList<Shape>
+    private lateinit var adapter: MyCustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        shapeArrayList = ArrayList()
+
+        val shape1: Shape = Shape("Sphere", R.drawable.sphere)
+        val shape2: Shape = Shape("Cylinder", R.drawable.cylinder)
+        val shape3: Shape = Shape("Cube", R.drawable.cube)
+        val shape4: Shape = Shape("Prism", R.drawable.prism)
+
+
+
+        shapeArrayList.add(shape1)
+        shapeArrayList.add(shape2)
+        shapeArrayList.add(shape3)
+        shapeArrayList.add(shape4)
+
+        adapter = MyCustomAdapter(this,shapeArrayList)
+
+        binding.apply {
+            gridView.adapter = adapter
+            gridView.numColumns = 2
+        }
+
     }
 }
